8ac04aa178d79c6ea3ba0ed4462d6ef6f256526b
Abdul Muksith <106472158+MuksithMRA@users.noreply.github.com>
2022-11-05 20:24:33 +0530

    bug fix

--- api/models/Dashboard.php
+++ api/models/Dashboard.php
@@ -3,7 +3,7 @@
         
         private $conn;
         private $db_name = 'suhada_constructions';
-        private $tables = array('employees','projects','finance');
+        private $tables = array('employees','projects','finance','vehicles');
         public $message;
       
         
@@ -14,19 +14,20 @@
 
         public function getTableRowCounts()
         {
-            $query = "SELECT table_name, table_rows FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ?";
-            $stmt = $this->conn->prepare($query);
-            $stmt->bind_param('s', $this->db_name);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            if($result->num_rows > 0){
-                
-                while($row = $result->fetch_assoc()){
-                    $dataSet[] = $row;
-                }
+            $datacount = [];
+            for ($i=0; $i < count($this->tables); $i++) { 
+                $query = "SELECT COUNT(*) FROM " . $this->tables[$i];
+                $stmt = $this->conn->prepare($query);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $data = array("table_name"=>$this->tables[$i],"table_rows"=>$result->field_count);
+                $datacount[] = $data;
+            }
+          
+            if(count($datacount) > 0){
                 $stmt->close();
                 $this->message = array('message' => 'data found', 'status' => 200);
-                return $dataSet;
+                return $datacount;
             }else{
                 $stmt->close();
                 $this->message = array('message' => 'no data found', 'status' => 404);
